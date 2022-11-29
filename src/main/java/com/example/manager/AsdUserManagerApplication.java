dc604f17f372b21f0c0d8b5ac0702518b85ee6c8
tomigithub12 <gaurlalhothi@hotmail.com>
2022-11-29 18:28:03 +0100

    First Commit- Project Setup

---
+++ src/main/java/com/example/manager/AsdUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdUserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsdUserManagerApplication.class, args);
+	}
+
+}
